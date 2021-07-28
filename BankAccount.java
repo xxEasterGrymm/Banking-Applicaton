a8267d6658ca855096733bb3764abe103226adf3
Arya Bhattacharyya <70765320+xxEasterGrymm@users.noreply.github.com>
2021-07-28 06:55:12 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -5,27 +5,32 @@ public class BankAccount {
     int previousTransaction;  //  the last amount which is withdrawn/deposited
     String customerName;
     String customerId;
+
     BankAccount(String cname, String cid) {
         customerName = cname;
         customerId = cid;
     }
+
     void deposit(int amount) {
         if(amount != 0) {
             balance = balance + amount;
             previousTransaction = amount;
         }
     }
+
     void withdraw(int amount) {
         if(amount != 0) {
             balance = balance - amount;
             previousTransaction = -amount;
         }
     }
+
     void getPreviousTransaction() {
         if(previousTransaction > 0) System.out.println("Deposited: " + previousTransaction);
         else if(previousTransaction < 0) System.out.println("Withdrawn: " + Math.abs(previousTransaction));
         else System.out.println("No transaction ocurred.");
     }
+
     void showMenu() {
         char option = '\0';
         System.out.println("Welcome " + customerName);
